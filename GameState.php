62a08485d5fa3e2067ba6ad00cda00b106e4cc8f
jjk4 <shaun7090@gmail.com>
2025-05-23 10:34:17 +0200

    creategame

--- GameState.php
+++ GameState.php
@@ -1,7 +1,8 @@
 <?php
 
-    class GameState{
+    class Game{
         private $gamestate;
+        private $gameid;
 
         public function __construct($gamestate=null){
             if(isset($gamestate)){
@@ -17,5 +18,31 @@
 
                 ];
             }
+        }
+        public function getGameState(): array{
+            return $this->gamestate;
+        }
+        private function toJson(): bool|string{
+            return json_encode($this->gamestate);
+        }
+        public function saveNew(){
+            global $db;
+            if(!isset($this->gameid)){ // Neues Spiel erstellen
+                $this->gameid = rand(min: 100000, max: 999999);
+                $stmt = $db->prepare(query: "INSERT INTO `games` (`id`, `last_used`, `game_data`) VALUES ('?', current_timestamp(), '?')");
+                if($stmt === false){
+                    return false;
+                }
+                $json = $this->toJson();
+                if($json === false){
+                    return false;
+                }
+                $stmt->bind_param("is", $this->gameid, $json);
+                if($stmt->execute() === false){
+                    return false;
+                }
+                return $this->gameid;;
+            }
+
         }
     }
--- index.php
+++ index.php
@@ -5,5 +5,25 @@ require_once('config.php');
 $db = new mysqli(hostname: $db_host, username: $db_user, password: $db_pass, database: $db_name, port: $db_port);
 
 if ($db->connect_error) {
-    die("Connection failed: " . $db->connect_error);
+    echo json_encode(["error" => "DB Connection failed"]);
+    die();
+}
+
+if(!isset($_GET["action"])){
+    echo json_encode(["error" => "No action specified"]);
+    die();
+}
+
+switch($_GET["action"]){
+    case "createGame":
+        $game = new Game();
+        $result = $game->saveNew();
+        if($result === false){
+            echo json_encode(["error" => "Failed to create game"]);
+            die();
+        } else {
+            echo json_encode(["gameid" => $result]);
+        }
+        break;
+
 }
